f0c049252c0ef58f78d9eacc03334cbf41a1e6c3
Max Engelhardt <maxengelhardt4@gmail.com>
2021-03-26 16:07:39 +0000

    Add deploy script

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,10 @@
 module.exports = {
-  siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
-  },
+    pathPrefix: '/gatsby-gitpod',
+    siteMetadata: {
+        title: `Gatsby Default Starter`,
+        description: `Kick off your next, ...`,
+        author: `@gatsbyjs`,
+    },
   plugins: [
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-image`,
